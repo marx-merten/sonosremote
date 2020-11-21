d9129617e56be9111ec6a9a19abb632018f9ae69
Stefan Marx <stefan@marx-merten.de>
2020-11-21 17:22:52 +0100

    basic fonts test

--- pythonfirmware/pysrc/live/test/fonts.py
+++ pythonfirmware/pysrc/live/test/fonts.py
@@ -1,11 +1,11 @@
 from lib.graphics.font import FontRegistry, FontInfo
-from lib.graphics import BufferedCanvas
+from lib.graphics import BufferedCanvas, ASCII_SEED
 from lib.graphics.debug import dump_buffer
 import fonts.basic
 import fonts.fontello
 
 r = FontRegistry()
-r.register_font("normal_12", fonts.basic.u8g2_font_ncenR12_tf)
+r.register_font("normal_12", fonts.basic.u8g2_font_ncenR24_tf)
 r.register_font("bold_12", fonts.basic.u8g2_font_ncenB12_tf)
 r.register_font("icons", fonts.fontello.fontello_icons_icon_24)
 f = r.get("normal_12")
@@ -31,3 +31,10 @@ def testChar(font: FontInfo, c):
 
         glyph.materialize(bb)
         dump_buffer(bb, glyph.width, glyph.height)
+
+
+def findMissing(font: FontInfo, search=ASCII_SEED):
+    for c in search:
+        g = font.get_glyph(c)
+        if not g:
+            print("Code Missing in Fone {} --[{}]".format(font.name, c))
