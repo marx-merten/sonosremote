3572881447a4a07e57a9eea0aadab2be0e8735ee
Stefan Marx <stefan@marx-merten.de>
2020-11-22 14:38:29 +0100

    better dirty check on syncfs

--- pythonfirmware/boardSync.sh
+++ pythonfirmware/boardSync.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+oldmd5=$(cat .life_md5)
+newmd5=$(find pysrc/live -type f -exec md5sum '{}' ';' | md5sum | awk '{print$1}')
+if [[ "$newmd5" != "$oldmd5" ]]; then
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ${DIR}/boardShell.sh rsync -m  $DIR/pysrc/live /pyboard
+fi
+echo "$newmd5">.life_md5
