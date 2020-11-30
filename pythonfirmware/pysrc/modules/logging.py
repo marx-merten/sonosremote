1fcd3f6204958edccf36ddf54527d74da5639b31
Stefan Marx <stefan@marx-merten.de>
2020-11-30 08:00:37 +0100

    updated Logging

--- pythonfirmware/pysrc/modules/logging.py
+++ pythonfirmware/pysrc/modules/logging.py
@@ -1,30 +1,99 @@
-import utime
+import sys
 
-LOG_NONE = const(0)
-LOG_TRACE = const(1)
-LOG_DEBUG = const(2)
-LOG_WARN = const(3)
-LOG_ERROR = const(4)
-LOG_FATAL = const(5)
+CRITICAL = 50
+ERROR = 40
+WARNING = 30
+INFO = 20
+DEBUG = 10
+NOTSET = 0
 
-logger = {}
-log_level = LOG_DEBUG
+_level_dict = {
+    CRITICAL: "CRIT",
+    ERROR: "ERROR",
+    WARNING: "WARN",
+    INFO: "INFO",
+    DEBUG: "DEBUG",
+}
+
+_stream = sys.stderr
 
 
 class Logger:
-    def __init__(self, name: string):
+
+    level = NOTSET
+
+    def __init__(self, name):
         self.name = name
-        logger[name] = self
 
-    def active(self, level):
-        return level >= log_level
+    def _level_str(self, level):
+        l = _level_dict.get(level)
+        if l is not None:
+            return l
+        return "LVL%s" % level
 
-    def is_debug(self):
-        return self.active(LOG_DEBUG)
+    def setLevel(self, level):
+        self.level = level
 
-    def is_trace(self):
-        return self.active(LOG_TRACE)
+    def isEnabledFor(self, level):
+        return level >= (self.level or _level)
 
-    def log(self, msg, level=LOG_DEBUG):
-        if self.active(level):
-            print(msg)
+    def log(self, level, msg, *args):
+        if level >= (self.level or _level):
+            _stream.write("%s:%s:" % (self._level_str(level), self.name))
+            if not args:
+                print(msg, file=_stream)
+            else:
+                print(msg % args, file=_stream)
+
+    def debug(self, msg, *args):
+        self.log(DEBUG, msg, *args)
+
+    def info(self, msg, *args):
+        self.log(INFO, msg, *args)
+
+    def warning(self, msg, *args):
+        self.log(WARNING, msg, *args)
+
+    def error(self, msg, *args):
+        self.log(ERROR, msg, *args)
+
+    def critical(self, msg, *args):
+        self.log(CRITICAL, msg, *args)
+
+    def exc(self, e, msg, *args):
+        self.log(ERROR, msg, *args)
+        sys.print_exception(e, _stream)
+
+    def exception(self, msg, *args):
+        self.exc(sys.exc_info()[1], msg, *args)
+
+
+_level = INFO
+_loggers = {}
+
+
+def getLogger(name):
+    if name in _loggers:
+        return _loggers[name]
+    l = Logger(name)
+    _loggers[name] = l
+    return l
+
+
+def info(msg, *args):
+    getLogger(None).info(msg, *args)
+
+
+def debug(msg, *args):
+    getLogger(None).debug(msg, *args)
+
+
+def basicConfig(level=INFO, filename=None, stream=None, format=None):
+    global _level, _stream
+    _level = level
+    if stream:
+        _stream = stream
+    if filename is not None:
+        print("logging.basicConfig: filename arg is not supported")
+    if format is not None:
+        print("logging.basicConfig: format arg is not supported")
